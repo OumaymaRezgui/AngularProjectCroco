b3212577f7ccfab8f951049bd45b9aee0eaa7101
oumay1995 <rezguioumayma95@gmail.com>
2022-09-24 12:54:43 +0100

    se connecter avec un client existant

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm : FormGroup;
+  client : any = {};
+  exists : boolean = true;
+  constructor(private formBuilder : FormBuilder , private router : Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email : [''],
+      password : [''],
+    })
+  }
+  
+  login(){
+    let   findedUser;
+    console.log(this.client)
+    let users = JSON.parse(localStorage.getItem("users") || "[]")
+    for (let i = 0; i < users.length; i++) {
+         if(users[i].email == this.client.email && users[i].password == this.client.password){
+          findedUser = users[i];
+          break;
+         }    
+    }
+    if(findedUser){
+      //user exist
+      localStorage.setItem("connectedUser",JSON.stringify(findedUser));
+     switch (findedUser.role) {
+      case 'admin':
+        this.router.navigate(["dashboardAdmin"]);
+      break;
+      case 'chef':
+        this.router.navigate(["dashboardchef"]);
+      break;
+      case 'client':
+        this.router.navigate([""]);
+       break;
+     }
+    }else{
+      //user not exist
+      this.exists = false;
+    }
   }
 
 }
